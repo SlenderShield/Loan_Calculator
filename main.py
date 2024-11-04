b5b9cdee52d3d287b7fdbb2d5c114f7c456576ec
Muralidhara Bhat K S <ksmuralidhara0@gmail.com>
2024-11-04 15:17:23 +0530

    add args parser

--- main.py
+++ main.py
@@ -1,34 +1,68 @@
 import math
 import argparse
 
-loan_principal = int(input("Enter the loan principal:"))
+parser = argparse.ArgumentParser(description="Loan Calculator.")
 
-print(
-    """What do you want to calculate?
-type "m" - for number of monthly payments,
-type "p" - for the monthly payment:"""
-)
-value = input()
+parser.add_argument("-prin", "--principal",
+                    help="You need to give principal amount.")
+parser.add_argument("-pay", "--payment",
+                    help="You need to give payment amount.")
+parser.add_argument("-per", "--periods",
+                    help="You need to give number of months.")
+parser.add_argument("-i", "--interest",
+                    help="You need to give the interest amount.")
+
+args = parser.parse_args()
 
 
-def calc_num_months(amount):
-    months = math.ceil(loan_principal / amount)
-    print(f"It will take {months} month to repay the loan")
+def find_periods(prin, pay, inter):
+    calc_value_for_payments = pay / (pay - (inter * prin))
 
+    num_of_payments = math.ceil(math.log(calc_value_for_payments, 1 + inter))
 
-def calc_pay_monthly(months):
-    if loan_principal % months == 0:
-        payment = loan_principal / months
-        print(f"Your monthly payment = {payment}")
+    if num_of_payments > 12:
+        years = num_of_payments // 12
+        months = num_of_payments % 12
+        if months > 0:
+            print(f"It will take {years} years and {months} months to repay this loan!")
+        else:
+            print(f"It will take {years} years to repay this loan!")
     else:
-        payment = math.ceil(loan_principal / months)
-        last = loan_principal - (months - 1) * payment
-        print(f"Your monthly payment = {payment} and the last payment = {last}.")
+        print(f"It will take {num_of_payments} months to repay this loan!")
 
 
-if value == "m":
-    monthly = int(input("Enter the monthly payment:"))
-    calc_num_months(monthly)
-elif value == "p":
-    numOfMonths = int(input("Enter the number of months:"))
-    calc_pay_monthly(numOfMonths)
+def calc_payments(prin, peri, inter):
+    calc_value = math.pow(1 + inter, peri)
+    pay = math.ceil(prin * (inter * calc_value) / (calc_value - 1))
+    print(f"Your monthly payment = {pay}!")
+
+
+def calc_principal(pay, peri, inter):
+    calc_value = math.pow((1 + inter), peri)
+
+    calc_time = (inter * calc_value) / (calc_value - 1)
+
+    prin = pay / calc_time
+    print(f"Your loan principal = {prin}!")
+
+
+if args.periods is None:
+    interest = float(args.interest)
+    principal = float(args.principal)
+    payment = float(args.payment)
+    interest_value = interest / (100 * 12)
+    find_periods(principal, payment, interest_value)
+
+if args.payment is None:
+    interest = float(args.interest)
+    principal = float(args.principal)
+    periods = float(args.periods)
+    interest_value = interest / (100 * 12)
+    calc_payments(principal, periods, interest_value)
+
+if args.principal is None:
+    interest = float(args.interest)
+    payment = float(args.payment)
+    periods = float(args.periods)
+    interest_value = interest / (100 * 12)
+    calc_principal(payment, periods, interest_value)
